7839fd57641e02701f4d8ee71d9d0da36cfcc3f7
Tibor <36704519+tibor3741@users.noreply.github.com>
2024-01-12 15:28:11 +0100

    First Microsoft.PowerShell_profile.ps1

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,8 @@
+CLEAR-HOST
+$Host.UI.RawUI.WindowTitle = "PowerShell"
+Set-Alias -Name help -Value Get-help
+Set-Alias -Name notepad -Value 'C:\Program Files\Notepad++\notepad++.exe'
+GET-Host
+HOSTNAME
+Get-Date -f "yyyy. MMMM dd., dddd HH:mm:ss"
+HOTFIX | Format-Table
