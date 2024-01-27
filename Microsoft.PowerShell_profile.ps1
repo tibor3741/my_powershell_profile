a6241f554330605bfa7504255883389084844ebf
Tibor <36704519+tibor3741@users.noreply.github.com>
2024-01-27 01:18:57 +0100

    Update Microsoft.PowerShell_profile.ps1
    Coment and new funcions and character-setup

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,8 +1,27 @@
+#My Powershell Profile
+#Screen Clean Setup
 CLEAR-HOST
-$Host.UI.RawUI.WindowTitle = "PowerShell"
+#Window Setup
+$Host.UI.RawUI.WindowTitle = "Admin"
+#characters Setup to utf-8
+chcp 65001 | Out-Null
+#prompt setup
+function prompt {"$env:USERNAME@$env:COMPUTERNAME $((Get-Location).Path) > "}
+#Aliases setup
+Set-Alias -Name adb -Value 'D:\Android Tools\adb.exe'
 Set-Alias -Name help -Value Get-help
 Set-Alias -Name notepad -Value 'C:\Program Files\Notepad++\notepad++.exe'
-GET-Host
+Set-Alias docker-compose "docker-compose.exe"
+Set-Alias docker "docker.exe"
+Set-Alias activate ".\env\Scripts\Activate"
+#Favorite-Folders-Setup
+function devops{
+    set-location D:\Codes
+}
+function webs{
+    set-location c:\xampp\htdocs\webs
+}
+#Run to start
 HOSTNAME
-Get-Date -f "yyyy. MMMM dd., dddd HH:mm:ss"
-HOTFIX | Format-Table
+GET-DATE -f "yyyy. MMMM dd., dddd HH:mm:ss"
+HOTFIX | Sort-Object InstalledOn -Descending | Select-Object -Property Description, InstalledOn | Format-Table -AutoSize	
